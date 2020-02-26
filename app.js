dab088cc437c2a5474c3858637fc9e067d062ed3
Adrian <adrian.belver@etservices.eu>
2020-02-26 09:53:34 +0100

    Cors added

--- app.js
+++ app.js
@@ -3,62 +3,65 @@ let membersDao = require("./dao/membersDao.js");
 let excursionsDao = require('./dao/excursionsDao');
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
+const cors = require('cors');
 
 app.use(bodyParser.urlencoded({ extended: true })); app.use(bodyParser.json()); app.use(bodyParser.raw());
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/members/list', (req, res)=>{
+app.get('/members/list', cors(), (req, res)=>{
     membersDao.getAllMembers()
     .then(members=>res.send(members));
 })
 
-app.get('/excursions/list', (req, res)=>{
+app.get('/excursions/list', cors(), (req, res)=>{
     excursionsDao.getAllexcursions()
     .then(members=>res.send(members));
 })
 
 
-app.get("/members/:id", (req, res)=>{
+app.get("/members/:id", cors(), (req, res)=>{
     membersDao.getMemberById(req.params.id)
     .then(result=>res.send(result));
 })
 
-app.get("/excursions/:id", (req, res)=>{
+app.get("/excursions/:id", cors(), (req, res)=>{
     excursionsDao.getExcursionById(req.params.id)
     .then(result=>res.send(result));
 })
 
-app.get("/members/delete/:id", (req, res)=>{
+app.get("/members/delete/:id", cors(), (req, res)=>{
     membersDao.deleteMember(req.params.id)
     .then(result=>res.send(result));
 })
 
-app.get("/excursions/delete/:id", (req, res)=>{
+app.get("/excursions/delete/:id", cors(), (req, res)=>{
     excursionsDao.deleteExcursion(req.params.id)
     .then(result=>res.send(result));
 })
 
-app.post("/members", (req, res)=>{
+app.post("/members", cors(),  (req, res)=>{
     membersDao.addMember(req.body)
     .then(result=>res.send(result));
 })
 
-app.post("/excursions", (req, res)=>{
+app.post("/excursions", cors(), (req, res)=>{
     excursionsDao.addExcursion(req.body)
     .then(result=>res.send(result));
 })
 
-app.put("/members", (req, res)=>{
+app.put("/members", cors(),  (req, res)=>{
     membersDao.editMember(req.body)
     .then(result=>res.send(result));
 })
 
-app.put("/excursions", (req, res)=>{
+app.put("/excursions", cors(), (req, res)=>{
     excursionsDao.editExcursion(req.body)
     .then(result=>res.send(result));
 })
+
+app.options('*', cors());
 
 
--- dao/excursionsDao.js
+++ dao/excursionsDao.js
@@ -2,87 +2,118 @@
 
 var MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
+const cors = require('cors');
+
 
 var uri = "mongodb://localhost:27017/";
 
 function getAllexcursions() {
-    return new Promise((resolve, reject) => {
-      const client = new MongoClient(uri, { useUnifiedTopology: true });
-  
-      client.connect().then((client) => {
-        var db = client.db('mountaineering_club')
-  
-        db.collection('excursions').find().toArray(function (err, result) {
-          if (err) throw err;
-          resolve(result)
-        })
+  return new Promise((resolve, reject) => {
+    const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+    client.connect().then((client) => {
+      var db = client.db('mountaineering_club')
+
+      db.collection("excursions").aggregate(
+        [
+          {
+            $lookup: {
+              from: "members",
+              localField: "users_id",
+              foreignField: "_id",
+              as: "members_info"
+            }
+          }
+        ]
+      ).toArray(function (err, result) {
+        if (err) throw err;
+        resolve(result);
+
+      });
+    })
+  })
+}
+
+function getExcursionById(id) {
+  return new Promise((resolve, reject) => {
+    const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+    client.connect().then((client) => {
+      var db = client.db('mountaineering_club');
+
+      /*db.collection('excursions').findOne({ _id: ObjectID(id) }, function (err, result) {
+        if (err) throw err;
+        resolve(result);
+      })*/
+      db.collection("excursions").aggregate(
+        [
+          { $match: { _id: ObjectID(id)}},
+          {
+            $lookup: {
+              from: "members",
+              localField: "users_id",
+              foreignField: "_id",
+              as: "members_info"
+            }
+          }
+        ]
+      ).toArray(function (err, result) {
+        if (err) throw err;
+        resolve(result);
+
+      });
+    })
+  })
+}
+
+function addExcursion(excursion) {
+  return new Promise((resolve, reject) => {
+    const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+    client.connect().then((client) => {
+      var db = client.db('mountaineering_club');
+      excursion._id = ObjectID(excursion._id);
+      excursion.date = new Date(excursion.date);
+      excursion.users_id = excursion.users_id.map(id => ObjectID(id));
+      db.collection('excursions').insertOne((excursion), function (err, result) {
+        if (err) throw err;
+        resolve(result);
       })
     })
-  }
+  })
+}
 
-  function getExcursionById(id) {
-    return new Promise((resolve, reject) => {
-      const client = new MongoClient(uri, { useUnifiedTopology: true });
-  
-      client.connect().then((client) => {
-        var db = client.db('mountaineering_club');
-  
-        db.collection('excursions').findOne({ _id: ObjectID(id) }, function (err, result) {
-          if (err) throw err;
-          resolve(result);
-        })
+
+function editExcursion(excursion) {
+  return new Promise((resolve, reject) => {
+    const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+    client.connect().then((client) => {
+      var db = client.db('mountaineering_club');
+      excursion._id = ObjectID(excursion._id);
+      excursion.date = new Date(excursion.date);
+      excursion.users_id = excursion.users_id.map(id => ObjectID(id));
+      db.collection('excursions').updateOne({ _id: excursion._id }, { $set: excursion }, function (err, result) {
+        if (err) throw err;
+        resolve(result);
       })
     })
-  }
+  })
+}
 
-function addExcursion(excursion){
-    return new Promise((resolve, reject) =>{
-      const client = new MongoClient(uri, { useUnifiedTopology: true });
-  
-      client.connect().then((client) => {
-        var db = client.db('mountaineering_club');
-        excursion._id = ObjectID(excursion._id);
-        excursion.date = new Date(excursion.date);
-        excursion.users_id = excursion.users_id.map(id => ObjectID(id));
-        db.collection('excursions').insertOne((excursion), function (err, result) {
-          if (err) throw err;
-          resolve(result);
-        })
+function deleteExcursion(id) {
+  return new Promise((resolve, reject) => {
+    const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+    client.connect().then((client) => {
+      var db = client.db('mountaineering_club');
+
+      db.collection('excursions').deleteOne({ _id: ObjectID(id) }, function (err, result) {
+        if (err) throw err;
+        resolve(result);
       })
     })
-  }
+  })
+}
 
-
-  function editExcursion(excursion){
-    return new Promise((resolve, reject) =>{
-      const client = new MongoClient(uri, { useUnifiedTopology: true });
-  
-      client.connect().then((client) => {
-        var db = client.db('mountaineering_club');
-        excursion._id = ObjectID(excursion._id);
-        excursion.date = new Date(excursion.date);
-        excursion.users_id = excursion.users_id.map(id => ObjectID(id));
-        db.collection('excursions').updateOne({_id: excursion._id}, {$set: excursion}, function (err, result) {
-          if (err) throw err;
-          resolve(result);
-        })
-      })
-    })
-  }
-
-  function deleteExcursion(id){
-    return new Promise((resolve, reject) => {
-      const client = new MongoClient(uri, { useUnifiedTopology: true });
-  
-      client.connect().then((client) => {
-        var db = client.db('mountaineering_club');
-  
-        db.collection('excursions').deleteOne( { _id: ObjectID(id) }, function (err, result) {
-          if (err) throw err;
-          resolve(result);
-        })
-      })
-    })
-  }
-
-  module.exports = {addExcursion, editExcursion, deleteExcursion, getAllexcursions, getExcursionById}
+module.exports = { addExcursion, editExcursion, deleteExcursion, getAllexcursions, getExcursionById }
